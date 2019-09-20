50f9ff418f881bff678584613f93eb005f3ab3d6
Nhu Quynh Tran Ngoc <nhu-quynh.tranngoc@bollore.com>
2019-09-20 15:19:50 +0700

    init

---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="hello">
+    <h1>{{ book.name }}</h1>
+    <slot name="description">{{book.description}}</slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "block",
+  props: {
+    book: {
+      type: Object,
+      default: { id: 0, name: "", description: "" }
+    }
+  },
+  data() {
+    return {
+      book: this.book
+    };
+  }
+};
+</script>
+
